478cdf68fa00afcf43ab4f01ef347905e2c8425a
riadhabdouli <abdouliriadh1919@gmail.com>
2023-02-27 18:04:26 +0100

    first commit

---
+++ TodoListWebApi.Repository/Migrations/20230226171024_hhhh.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoListWebApi.Repository.Migrations
+{
+    public partial class hhhh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
